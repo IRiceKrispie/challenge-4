05b82ef9bd59766dae67827366d5a8d135198861
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2022-10-07 04:55:59 -0500

    trying to fix remove child bug

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,10 @@ var quizButton2 = document.createElement("button"); //create 2nd button list ele
 var quizButton3 = document.createElement("button"); //create 3rd button list element
 var form = document.querySelector(".results"); //point to our results form
 var submitBttn = document.querySelector("#submit-score"); //point to our submit button
+var initialsInput = document.querySelector("#input"); //points to our input field
+var restartBttn = document.querySelector("#restart-game");
 var score = 0;
+var totalTime = 200;
 
 function question1(){
     quizQuestion.textContent = "What is 2 + 2?"; //display the question
@@ -18,6 +21,8 @@ function question1(){
 
     quizButton3.textContent = "4";
     quizQuestion.appendChild(quizButton3);
+
+    console.log(quizQuestion);
     //user selects wrong answer
     quizButton.addEventListener("click", function(){
         quizQuestion.removeChild(quizButton);
@@ -47,21 +52,22 @@ function question1(){
         quizQuestion.textContent = "";
         question2();
     })
-
 }
-
 
 function question2(){
-    quizQuestion.textContent = "When was USA founded?";
-    quizButton.textContent = "2000";
+    quizQuestion.textContent = "What is 3 + 3?"; //display the question
+
+    quizButton.textContent = "3";
     quizQuestion.appendChild(quizButton);
 
-    quizButton2.textContent = "1776";
+    quizButton2.textContent = "5";
     quizQuestion.appendChild(quizButton2);
 
-    quizButton3.textContent = "1875";
+    quizButton3.textContent = "4";
     quizQuestion.appendChild(quizButton3);
 
+    console.log(quizQuestion);
+    //user selects wrong answer
     quizButton.addEventListener("click", function(){
         quizQuestion.removeChild(quizButton);
         quizQuestion.removeChild(quizButton2);
@@ -71,9 +77,6 @@ function question2(){
     })
 
     quizButton2.addEventListener("click", function(){
-        if(quizButton2.textContent === "1776"){
-            score += 1;
-        }
         quizQuestion.removeChild(quizButton);
         quizQuestion.removeChild(quizButton2);
         quizQuestion.removeChild(quizButton3);
@@ -81,66 +84,94 @@ function question2(){
         question3();
     })
 
+    //user selects correct answer
     quizButton3.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question3();
-    })
-
-}
-
-function question3(){
-    quizQuestion.textContent = "what is the first letter in the English Alphabet?";
-
-    quizButton.textContent = "A";
-    quizQuestion.appendChild(quizButton);
-
-    quizButton2.textContent = "B";
-    quizQuestion.appendChild(quizButton2);
-
-    quizButton3.textContent = "C";
-    quizQuestion.appendChild(quizButton3);
-
-    quizButton.addEventListener("click", function(){
-        if(quizButton.textContent === "A"){
+        if(quizButton3.textContent === "4"){
             score += 1;
         }
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        results();
-    })
-
-    quizButton2.addEventListener("click", function(){
         
         quizQuestion.removeChild(quizButton);
         quizQuestion.removeChild(quizButton2);
         quizQuestion.removeChild(quizButton3);
         quizQuestion.textContent = "";
-        results();
+        question3();
     })
+}
 
-    quizButton3.addEventListener("click", function(){
+function question3(){
+    console.log(quizQuestion.innerHTML);
+    quizQuestion.textContent = "What is 5+5?"; //display the question
+
+    quizButton.textContent = "3";
+    quizQuestion.appendChild(quizButton);
+
+    quizButton2.textContent = "5";
+    quizQuestion.appendChild(quizButton2);
+
+    quizButton3.textContent = "4";
+    quizQuestion.appendChild(quizButton3);
+
+    console.log(quizQuestion);
+    //user selects wrong answer
+    quizButton.addEventListener("click", function(){
         quizQuestion.removeChild(quizButton);
         quizQuestion.removeChild(quizButton2);
         quizQuestion.removeChild(quizButton3);
         quizQuestion.textContent = "";
-        results();
     })
 
+    quizButton2.addEventListener("click", function(){
+        quizQuestion.removeChild(quizButton);
+        quizQuestion.removeChild(quizButton2);
+        quizQuestion.removeChild(quizButton3);
+        quizQuestion.textContent = "";
+    })
+
+    //user selects correct answer
+    quizButton3.addEventListener("click", function(){
+        if(quizButton3.textContent === "4"){
+            score += 1;
+        }
+        
+        quizQuestion.removeChild(quizButton);
+        quizQuestion.removeChild(quizButton2);
+        quizQuestion.removeChild(quizButton3);
+        quizQuestion.textContent = "";
+    })
 }
+
+
+
+
+
 
 function results(){
     form.style.display = 'flex';
+    var displayScore = document.querySelector("#score");
+    displayScore.textContent = "Input Initials";
+    submitBttn.addEventListener("click", function(){
+        displayScore.textContent = "Player: " + initialsInput.value + " Score: " + score ;
+    })
+
+    initialsInput.addEventListener("keydown", function(event){
+        var key = event.key;
+        if (key === "Enter"){
+            event.preventDefault();
+            displayScore.textContent = "Player: " + initialsInput.value + " Score: " + score ;
+        }
+    })
+
+    restartBttn.addEventListener("click", function(event){
+        score = 0;
+        form.style.display = 'none';
+        buildQuiz();
+    })
 }
 
 
 function buildQuiz(){
-    //question1();
-    results();
+    question1();
+    //results();
 }
 
-var init = buildQuiz();
+buildQuiz();
