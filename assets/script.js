ddd63d2f162526cb713bf6f3e79f2d162ace69f2
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2022-10-07 05:55:25 -0500

    changed logic of script file

--- assets/script.js
+++ assets/script.js
@@ -1,177 +1,46 @@
-var quizCard = document.querySelector(".quiz-card"); //point to our section with the quiz card class
-var quizQuestion = document.querySelector("#quiz"); //point to our div with the quiz question
-var quizButton = document.createElement("button"); //create button list element
-var quizButton2 = document.createElement("button"); //create 2nd button list element
-var quizButton3 = document.createElement("button"); //create 3rd button list element
-var form = document.querySelector(".results"); //point to our results form
-var submitBttn = document.querySelector("#submit-score"); //point to our submit button
+var quizQuestionsContainer = document.querySelector("#quiz"); //point to our div with the quiz questions
+var resultsPage = document.querySelector("#results-page");//point to our results page form
+var restartBttn = document.querySelector("#restart-game");//point to our reset game button
+var submitBttn = document.querySelector("#submit-score");//point to our submit initials button
+var question1 = document.querySelector("#question1"); //point to our first question
+var question2 = document.querySelector("#question2"); //point to our second question
+var question3 = document.querySelector("#question3"); //point to our third question
+var q1a1Bttn = document.querySelector("#q1a1");//point to first button in first question
+var q2a2Bttn = document.querySelector("#q2a2");//point to second button in second question
+var q3a1Bttn = document.querySelector("#q3a1");//point to first button in third question
 var initialsInput = document.querySelector("#input"); //points to our input field
-var restartBttn = document.querySelector("#restart-game");
-var score = 0;
-var totalTime = 200;
-
-function question1(){
-    quizQuestion.textContent = "What is 2 + 2?"; //display the question
-
-    quizButton.textContent = "3";
-    quizQuestion.appendChild(quizButton);
-
-    quizButton2.textContent = "5";
-    quizQuestion.appendChild(quizButton2);
-
-    quizButton3.textContent = "4";
-    quizQuestion.appendChild(quizButton3);
-
-    console.log(quizQuestion);
-    //user selects wrong answer
-    quizButton.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question2();
-    })
-
-    quizButton2.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question2();
-    })
-
-    //user selects correct answer
-    quizButton3.addEventListener("click", function(){
-        if(quizButton3.textContent === "4"){
-            score += 1;
-        }
-        
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question2();
-    })
-}
-
-function question2(){
-    quizQuestion.textContent = "What is 3 + 3?"; //display the question
-
-    quizButton.textContent = "3";
-    quizQuestion.appendChild(quizButton);
-
-    quizButton2.textContent = "5";
-    quizQuestion.appendChild(quizButton2);
-
-    quizButton3.textContent = "4";
-    quizQuestion.appendChild(quizButton3);
-
-    console.log(quizQuestion);
-    //user selects wrong answer
-    quizButton.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question3();
-    })
-
-    quizButton2.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question3();
-    })
-
-    //user selects correct answer
-    quizButton3.addEventListener("click", function(){
-        if(quizButton3.textContent === "4"){
-            score += 1;
-        }
-        
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-        question3();
-    })
-}
-
-function question3(){
-    console.log(quizQuestion.innerHTML);
-    quizQuestion.textContent = "What is 5+5?"; //display the question
-
-    quizButton.textContent = "3";
-    quizQuestion.appendChild(quizButton);
-
-    quizButton2.textContent = "5";
-    quizQuestion.appendChild(quizButton2);
-
-    quizButton3.textContent = "4";
-    quizQuestion.appendChild(quizButton3);
-
-    console.log(quizQuestion);
-    //user selects wrong answer
-    quizButton.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-    })
-
-    quizButton2.addEventListener("click", function(){
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-    })
-
-    //user selects correct answer
-    quizButton3.addEventListener("click", function(){
-        if(quizButton3.textContent === "4"){
-            score += 1;
-        }
-        
-        quizQuestion.removeChild(quizButton);
-        quizQuestion.removeChild(quizButton2);
-        quizQuestion.removeChild(quizButton3);
-        quizQuestion.textContent = "";
-    })
-}
+var displayScore = document.querySelector("#score"); //points to our display score paragraph
 
 
+console.log(q1a1Bttn.textContent);
+q1a1Bttn.addEventListener("click", function(){
+    question1.classList.replace('active-question', 'not-active');
+    question2.classList.replace('not-active', 'active-question');
+})
 
+q2a2Bttn.addEventListener("click", function(){
+    question2.classList.replace('active-question', 'not-active');
+    question3.classList.replace('not-active', 'active-question');
+})
 
+q3a1Bttn.addEventListener("click", function(){
+    question3.classList.replace('active-question', 'not-active');
+    resultsPage.classList.replace('not-active', 'results');
+})
 
+restartBttn.addEventListener("click", function(){
+    resultsPage.classList.replace('results', 'not-active');
+    question1.classList.replace('not-active', 'active-question');
+})
 
-function results(){
-    form.style.display = 'flex';
-    var displayScore = document.querySelector("#score");
-    displayScore.textContent = "Input Initials";
-    submitBttn.addEventListener("click", function(){
-        displayScore.textContent = "Player: " + initialsInput.value + " Score: " + score ;
-    })
+submitBttn.addEventListener("click", function(){
+    displayScore.textContent = "Player: " + initialsInput.value;
+})
 
-    initialsInput.addEventListener("keydown", function(event){
-        var key = event.key;
-        if (key === "Enter"){
-            event.preventDefault();
-            displayScore.textContent = "Player: " + initialsInput.value + " Score: " + score ;
-        }
-    })
-
-    restartBttn.addEventListener("click", function(event){
-        score = 0;
-        form.style.display = 'none';
-        buildQuiz();
-    })
-}
-
-
-function buildQuiz(){
-    question1();
-    //results();
-}
-
-buildQuiz();
+initialsInput.addEventListener("keydown", function(event){
+    var key = event.key;
+    if (key === "Enter"){
+        event.preventDefault();
+        displayScore.textContent = "Player: " + initialsInput.value;
+    }
+})
